47d0d19583a1b237340eb9d7920cdf529db20385
shayanhuda0702 <shayanhuda0702@gmail.com>
2026-07-22 16:49:22 -0400

    fix hamburger menu

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('menu--open');
+  document.body.classList.toggle("menu--open");
 }
 
 function closeMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.remove('menu--open');
+  document.body.classList.remove("menu--open");
 }
-
