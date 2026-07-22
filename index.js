5cdcef520a6316f04421f051ab2225512dd184c2
shayanhuda0702 <shayanhuda0702@gmail.com>
2026-07-22 16:13:25 -0400

    create footer and Small screen opimization

---
+++ index.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('menu--open');
+}
+
+function closeMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.remove('menu--open');
+}
+
